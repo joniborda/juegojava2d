92134bca16d4f4684559f774590ef14b5a74688c
joniborda <jonathanmatiasborda@gmail.com>
2017-07-24 23:55:08 -0300

    empiezo con invertir sprite

--- src/graficos/HojaSprites.java
+++ src/graficos/HojaSprites.java
@@ -9,39 +9,40 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.imageio.ImageIO;
 
 /**
- *
+ * 
  * @author Joni
  */
 public class HojaSprites {
-    private final int ancho;
-    private final int alto;
-    public final int[] pixeles;
-    
-    // coleccion de hoja de sprites
-    public static HojaSprites desierto = new HojaSprites("/texturas/desierto.png", 320, 320);
-    
-    public HojaSprites(final String ruta, final int ancho, final int alto) {
-        this.ancho = ancho;
-        this.alto = alto;
-        pixeles = new int[ancho * alto];
-        
-        BufferedImage imagen;
-        try {
-            imagen = ImageIO.read(HojaSprites.class.getResource(ruta));
-            imagen.getRGB(0, 0, ancho, alto, pixeles, 0, ancho);
-        } catch (IOException ex) {
-            Logger.getLogger(HojaSprites.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    public int obtenAncho() {
-    	return this.ancho;
-    }
-        
-    public int obtenAlto() {
-    	return this.alto;
-    }
+	private final int ancho;
+	private final int alto;
+	public final int[] pixeles;
+
+	// coleccion de hoja de sprites
+	public static HojaSprites desierto = new HojaSprites("/texturas/desierto.png", 320, 320);
+
+	public HojaSprites(final String ruta, final int ancho, final int alto) {
+		this.ancho = ancho;
+		this.alto = alto;
+		pixeles = new int[ancho * alto];
+
+		BufferedImage imagen;
+		try {
+			imagen = ImageIO.read(HojaSprites.class.getResource(ruta));
+			imagen.getRGB(0, 0, ancho, alto, pixeles, 0, ancho);
+		} catch (IOException ex) {
+			Logger.getLogger(HojaSprites.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	public int obtenAncho() {
+		return this.ancho;
+	}
+
+	public int obtenAlto() {
+		return this.alto;
+	}
 }
--- src/graficos/Sprite.java
+++ src/graficos/Sprite.java
@@ -1,7 +1,7 @@
 package graficos;
 
 public class Sprite {
-	private final int LADO;
+	private final int lado;
 	private int x;
 	private int y;
 
@@ -10,51 +10,33 @@ public class Sprite {
 
 	// coleccion de sprites
 	public static final Sprite VACIO = new Sprite(32, 0x000000);
-	public static final Sprite ASFALTO = new Sprite(32, 0, 0,
-			HojaSprites.desierto);
-	public static final Sprite ARENA = new Sprite(32, 1, 0,
-			HojaSprites.desierto);
-	public static final Sprite BORDE_CARRETERA = new Sprite(32, 2, 0,
-			HojaSprites.desierto);
-	public static final Sprite CENTRO_CARRETERA = new Sprite(32, 3, 0,
-			HojaSprites.desierto);
-	public static final Sprite ESQUINA_CARRETERA = new Sprite(32, 4, 0,
-			HojaSprites.desierto);
-	public static final Sprite PARED_PIEDRA = new Sprite(32, 5, 0,
-			HojaSprites.desierto);
-	public static final Sprite PARED_PIEDRA_INFERIOR = new Sprite(32, 6, 0,
-			HojaSprites.desierto);
-	public static final Sprite PARED_PIEDRA_CARRETERA = new Sprite(32, 0, 3,
-			HojaSprites.desierto);
-	public static final Sprite PUERTA_SUPERIOR_IZQUIERDA = new Sprite(32, 7, 0,
-			HojaSprites.desierto);
-	public static final Sprite PUERTA_INTERMEDIA_IZQUIERDA = new Sprite(32, 7,
-			1, HojaSprites.desierto);
-	public static final Sprite PUERTA_INFERIOR = new Sprite(32, 8, 1,
-			HojaSprites.desierto);
-	public static final Sprite OXIDO = new Sprite(32, 9, 0,
-			HojaSprites.desierto);
-	public static final Sprite PUERTA_SUPERIOR_CENTRAL = new Sprite(32, 8, 0,
-			HojaSprites.desierto);
+	public static final Sprite ASFALTO = new Sprite(32, 0, 0, 0, HojaSprites.desierto);
+	public static final Sprite ARENA = new Sprite(32, 1, 0, 0, HojaSprites.desierto);
+	public static final Sprite BORDE_CARRETERA = new Sprite(32, 2, 0, 0, HojaSprites.desierto);
+	public static final Sprite CENTRO_CARRETERA = new Sprite(32, 3, 0, 0, HojaSprites.desierto);
+	public static final Sprite ESQUINA_CARRETERA = new Sprite(32, 4, 0, 0, HojaSprites.desierto);
+	public static final Sprite PARED_PIEDRA = new Sprite(32, 5, 0, 0, HojaSprites.desierto);
+	public static final Sprite PARED_PIEDRA_INFERIOR = new Sprite(32, 6, 0, 0, HojaSprites.desierto);
+	public static final Sprite PARED_PIEDRA_CARRETERA = new Sprite(32, 0, 3, 0, HojaSprites.desierto);
+	public static final Sprite PUERTA_SUPERIOR_IZQUIERDA = new Sprite(32, 7, 0, 0, HojaSprites.desierto);
+	public static final Sprite PUERTA_SUPERIOR_DERECHA = new Sprite(32, 7, 0, 1, HojaSprites.desierto);
+	public static final Sprite PUERTA_INTERMEDIA_IZQUIERDA = new Sprite(32, 7, 1, 0, HojaSprites.desierto);
+	public static final Sprite PUERTA_INFERIOR = new Sprite(32, 8, 1, 0, HojaSprites.desierto);
+	public static final Sprite OXIDO = new Sprite(32, 9, 0, 0, HojaSprites.desierto);
+	public static final Sprite PUERTA_SUPERIOR_CENTRAL = new Sprite(32, 8, 0, 0, HojaSprites.desierto);
 
-	public Sprite(final int lado, final int columna, final int fila,
-			final HojaSprites hoja) {
-		this.LADO = lado;
+	public Sprite(final int lado, final int columna, final int fila, final int version, final HojaSprites hoja) {
+		this.lado = lado;
 		this.pixeles = new int[lado * lado];
 		this.x = columna * lado;
 		this.y = fila * lado;
 		this.hoja = hoja;
 
-		for (int y = 0; y < lado; y++) {
-			for (int x = 0; x < lado; x++) {
-				pixeles[x + y * lado] = hoja.pixeles[(x + this.x)
-						+ (y + this.y) * hoja.obtenAncho()];
-			}
-		}
+		cargarSprite(version);
 	}
 
 	public Sprite(final int lado, final int color) {
-		this.LADO = lado;
+		this.lado = lado;
 		pixeles = new int[lado * lado];
 
 		for (int i = 0; i < pixeles.length; i++) {
@@ -63,7 +45,53 @@ public class Sprite {
 	}
 
 	public int obtenLado() {
-		return LADO;
+		return lado;
 	}
 
+	private void cargarSprite(int version) {
+		if (version == 0) {
+			cargaNormal();
+		} else {
+			cargarManipulada(version);
+		}
+	}
+
+	private void cargaNormal() {
+		for (int y = 0; y < lado; y++) {
+			for (int x = 0; x < lado; x++) {
+				pixeles[x + y * lado] = hoja.pixeles[(x + this.x) + (y + this.y) * hoja.obtenAncho()];
+			}
+		}
+	}
+
+	private void cargarManipulada(int version) {
+		int[] pixelesTemporales = iniciarPixelesTemporales();
+
+		switch (version) {
+			case 1 :
+				break;
+			case 2 :
+				break;
+			case 3 :
+				break;
+			case 4 :
+				break;
+			case 5 :
+				break;
+			case 6 :
+				break;
+			case 7 :
+				break;
+		}
+
+	}
+	private int[] iniciarPixelesTemporales() {
+		int[] pixelesTemporales = new int[lado * lado];
+		for (int y = 0; y < lado; y++) {
+			for (int x = 0; x < lado; x++) {
+				pixelesTemporales[x + y * lado] = hoja.pixeles[(x + this.x) + (y + this.y) * hoja.obtenAncho()];
+			}
+		}
+		return pixelesTemporales;
+	}
 }
